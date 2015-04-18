3f890a919409644a2fad500544c69c6b640e62ff
taho101 <tahirski@gmail.com>
2015-04-18 21:14:54 +0200

    Initial Commit
    Added first details to the application. It is still testing for
    potential usage.

---
+++ ComicReader/DirectorySelect.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms; 
+
+namespace ComicReader
+{
+    /// <summary>
+    /// Interaction logic for DirectorySelect.xaml
+    /// </summary>
+    public partial class DirectorySelect : Window
+    {
+        public DirectorySelect()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenFolderDialog_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.TextBoxFolderPath.Text = dialog.SelectedPath;
+
+                Properties.Settings.Default["root_dir"] = dialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
---
+++ ComicReader/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicReader.ViewModels
+{
+    public class LibraryViewModel : ViewModelBase
+    {
+        private ObservableCollection<string> _covers = new ObservableCollection<string> { 
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.blueboxsports.com/bbs/wp-content/uploads/2013/09/v5feat_200-comic-book-heroe.jpg",
+            "http://assets.nydailynews.com/polopoly_fs/1.45607.1313760142!/img/httpImage/image.jpg_gen/derivatives/gallery_1200/gal-comics-amazing-fantasy-15-jpg.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg",
+            "http://www.wired.com/images_blogs/underwire/2009/12/1982-wolverine-1-4.jpg"
+        };
+
+        public ObservableCollection<string> Covers
+        {
+            get { return _covers; }
+            set { SetProperty(ref _covers, value, "Covers");  }
+        }
+    }
+}
